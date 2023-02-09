1c5f6fb6a18684f81125c27919c5bf21303bf036
shraavyakunch21 <121798070+shraavyakunch21@users.noreply.github.com>
2023-02-09 12:30:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ChangePassword from "./components/pages/auth/ChangePassword";
-import LoginReg from "./components/pages/auth/LoginReg";
-import ResetPassword from "./components/pages/auth/ResetPassword";
-import SendPasswordResetEmail from "./components/pages/auth/SendPasswordRestEmail";
-import Dashboard from "./components/pages/Dashboard";
-import Home from "./components/pages/Home";
-import Layout from "./components/pages/Layout";
 function App() {
   return (
-    <>
-    
-      <BrowserRouter>
-        <Routes>
-        <Route index element={<Home />} />
-        
-          <Route path="/" element={<Layout />}>
-            <Route path="login" element={<LoginReg />} />
-            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
-            <Route path="reset" element={<ResetPassword />} />
-          </Route>
-          <Route path="/ChangePassword" element={<ChangePassword />} />
-          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
-
